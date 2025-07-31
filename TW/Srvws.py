591511d24c8d778532c20b73f2085b3d61fce194
Olivetta21 <ivancraftbr@gmail.com>
2025-07-31 08:00:43 -0400

    first communication

--- TW/Srvws.py
+++ TW/Srvws.py
@@ -5,25 +5,47 @@ import json
 
 class Player:
     serial = 0
-    def nextSerial(self):
-        self.serial += 1
-        return self.serial
+    def nextSerial():
+        Player.serial += 1
+        return Player.serial
 
     def __init__(self, ws):
         self.ws = ws
         self.name = None
 
-
 class Room:
     serial = 0
-    def nextSerial(self):
-        self.serial += 1
-        return self.serial
-    
+    def nextSerial():
+        Room.serial += 1
+        return Room.serial
+
     def __init__(self):
         self.msgs = asyncio.Queue()
         self.users = set()
 
+players = {}
+rooms = {}
 
+async def hp_messages(pid):
+    p = players[pid]
+    async for msg in p.ws:
+        if msg == "ping":
+            await p.ws.send("pong")
 
-if __name__ == "__main__":
+async def handler(ws):
+    pid = Player.nextSerial()
+    players[pid] = Player(ws)
+
+    try:
+        print(f"Player {pid} connected")
+        await hp_messages(pid)
+    finally:
+        del players[pid]
+        print(f"Player {pid} disconnected")
+
+async def main():
+    server = await websockets.serve(handler, "localhost", 8765)
+    print("Server started")
+    await server.wait_closed()
+
+asyncio.run(main())
